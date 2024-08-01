75020db39bb9a0eedf9cc12a9292f2bad8417e39
WAHU0508 <sharongikenye@gmail.com>
2024-08-01 11:50:55 +0300

    Add event listeners

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    const focused = () => {console.log('Good!')}
+    const blurred = () => {console.log('Hey! Eyes on me!')}
+    return (
+        <button onFocus={focused} onBlur={blurred}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
